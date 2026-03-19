963df8e4e73fff59dfef873a5dfccaa3049178fc
Omar Lozano <omar.yesid1004@gmail.com>
2026-03-19 13:47:01 -0500

    cm: add CORS config

---
+++ task-manager/src/main/java/co/com/olozano/taskmanager/Security/CorsConfig.java
@@ -0,0 +1,24 @@
+package co.com.olozano.taskmanager.Security;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
